004f8331ace7102ee86c9a6d6f57a22346fbd641
daniyalkhan131 <daniyalkhan131@gmail.com>
2024-05-14 17:40:01 +0530

    on local machine full api created

--- DLdeployment_API/service/api/endpoints/detect.py
+++ DLdeployment_API/service/api/endpoints/detect.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter, UploadFile, HTTPException
 from PIL import Image
 from io import BytesIO
 import numpy as np
+from service.core.logic.onnx_inference import emotions_detector
 
 emo_router = APIRouter() #for routing info. from here to main.py
 
 @emo_router.post("/detect")
 def detect (im: UploadFile): #it is for uploading file
 
-    if im. filename. split(". ")[-1] in ("jpg", "jpeg", "png"):
+    if im.filename.split(".")[-1] in ("jpg", "jpeg", "png"):
         pass 
     else: 
         raise HTTPException (
@@ -17,4 +18,4 @@ def detect (im: UploadFile): #it is for uploading file
     image=Image.open(BytesIO(im.file.read())) #for reading the image
     image=np.array(image)
 
-    return emotion_detector(image)
+    return emotions_detector(image)
--- DLdeployment_API/service/main.py
+++ DLdeployment_API/service/main.py
@@ -3,6 +3,11 @@ from service.api.api import main_router
 
 app = FastAPI (project_name='Diabetic-Retinopathy-Detection')
 app.include_router(main_router)
+ 
+
+@app.get("/")
+def root():
+    return {'welcome to diabetic retinopathy diagnosis, images': 'grading' }
 
 
 # class InputItem(BaseModel):
